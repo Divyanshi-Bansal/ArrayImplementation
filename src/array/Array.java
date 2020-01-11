7bf5c023a90ad4d05f81f5c2ae3b7d5add949488
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-01-11 12:18:02 +0530

    Initial commit

---
+++ src/array/Array.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Array {
+    private int length;
+    private int index=0;
+    int [] arr;
+    public Array(int length){
+        this.length=length;
+        arr =new int[length];
+    }
+
+    public void insert(int element){
+        arr[index] = 
+    }
+
+}
