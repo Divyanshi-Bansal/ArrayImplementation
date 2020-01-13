6634a63d2a37f3d97897f208f20cb2caeeb9959d
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-01-13 16:21:59 +0530

    adding bubble sort method adding printvalue method

--- src/array/Array.java
+++ src/array/Array.java
@@ -1,9 +1,14 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array<arr> {
     private int length;
     private int index=0;
-    int [] arr;
+    public int [] arr;
+    public int s=0;
+
+
     public Array(int length){
         this.length=length;
         arr =new int[length];
@@ -19,7 +24,7 @@ public class Array<arr> {
         }
     }
     
-    public void deleteValue(){
+    public void deleteValue(int index){
         if(index>=0){
             arr[--index]=0;
         }
@@ -35,4 +40,38 @@ public class Array<arr> {
         }
         System.out.println("]");
     }
+
+    public void bubbleSort(int[] arr){
+        for(int j=0;j<arr.length-j;j++){//function of bubble sort
+            for(int i=0;i<j;i++){
+                if(arr[i]>arr[i+1]){
+                    swapArrayValues(i,i+1,arr);
+                }
+            }
+        }
+//        int k =Arrays.sort(arr);//utilise method
+//        System.out.println(k);
+
+    }
+
+    private void swapArrayValues(int i, int i1,int[]arr) {
+        int temp =arr[1];
+        arr[i]=arr[i1];
+        arr[i1]=temp;
+    }
+
+
+    //public void sortArray(){
+        //Arrays.sort(arr);
+        //System.out.println("modified arr[] : %s", Arrays.toString(arr));
+    //}
+
+    public void sumArray(){
+        for (int i = 0; i < length; i++) {
+            s=s+arr[i];
+        }
+        System.out.println(s);
+    }
+
+    public  void
 }
