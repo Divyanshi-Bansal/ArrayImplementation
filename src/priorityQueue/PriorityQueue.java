30934003fb294f677d02e6f961ac74ffed2901a3
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-19 04:49:34 +0530

    declare the variable

--- src/priorityQueue/PriorityQueue.java
+++ src/priorityQueue/PriorityQueue.java
@@ -1,5 +1,11 @@
 package priorityQueue;
 
 public class PriorityQueue {
+    private int front;
+    private int rear;
+    private int[] arr;
+    private int size;
+    private int maxSize;
+
 
 }
---
+++ src/test/codechef.java
@@ -0,0 +1,9 @@
+package test;
+
+import java.util.Scanner;
+
+public class codechef {
+    Scanner scanner = new Scanner ( System.in );
+    int t= scanner.nextInt ();
+
+}
